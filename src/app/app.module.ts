0b3ab2d8c20c6af0c675a19aba80ecc707f1e868
Ion Popa <ipopa@vivido.it>
2024-08-05 15:26:06 +0200

    Fix navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeTwoComponent } from './components/home-two/home-two.component';
-import { PreloaderComponent } from './components/preloader/preloader.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PreloaderComponent,
     HomeTwoComponent
   ],
   imports: [
